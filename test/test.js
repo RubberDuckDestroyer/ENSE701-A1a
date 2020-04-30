e401672c8d054be72afbc9686ea524b25cfcacfe
RubberDuckDestroyer <hitarthasrani@gmail.com>
2020-04-30 13:43:16 +1200

    Added unit tests

--- test/test.js
+++ test/test.js
@@ -1,39 +1,17 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'test';
-const dotenv = require('dotenv').config();
-var keystone = require('@keystonejs/keystone');
-var chai = require('chai');
-var dbURI = process.env.MONGO_URL
+const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
+const { keystone } = require('../index')
 
-keystone.init({
-  'name': 'Post Model Test'
-});
-
-var Post = null;
-
-
-keystone.import('../../models');
-
-chai.should();
-
-describe('Posts', function() {
-  beforeEach(function(done){
-    if (keystone.mongoose.connection.db) return done();
-    console.log('Connecting to ' + dbURI)
-    keystone.mongoose.connect(dbURI, done);
+var assert = require('assert');
+describe('Keystone cms', function() {
+  describe('Mongo Connection', function() {
+    it('should connect to MongoDB', function() {
+      assert.equal(process.env.CONNECT_TO, keystone.adapters.MongooseAdapter.config.mongoUri);
+    });
   });
 
-  it('should be a connection to Mongo', function(done){
-    keystone.mongoose.connection.db.should.be.a('Object');
-    done();
-  });
-
-  it('should be a Mongoose Model', function(done) {
-    Post = keystone.list('Post');
-
-    Post.should.be.a('Object');
-    Post.should.have.property('model').be.a('Function');
-    Post.should.have.property('schema').be.a('Object');
-
-    done();
-  });
+  describe('Keystone', function() {
+    it('should be an object',function() {
+      assert.equal(typeof(keystone),typeof({}));
+    });
+  })
 });
